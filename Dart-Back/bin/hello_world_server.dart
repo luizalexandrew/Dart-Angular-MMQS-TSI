132c0ba8d76389747a5b93fb07d4b7b6cc47e873
luizalexandrew <luiz.alexandre@live.com>
2016-06-20 03:37:56 -0300

    Front exemple use dart

--- Dart-Back/bin/hello_world_server.dart
+++ Dart-Back/bin/hello_world_server.dart
@@ -6,7 +6,18 @@ main() async {
     await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 4000 );
   	print('Rodando na porta, port ${requestServer.port}');
 
+
+
+
+
+
   	await for (HttpRequest request in requestServer) {
+
+
+      request.response.headers.add("Access-Control-Allow-Origin", "*");
+      request.response.headers.add("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT,OPTIONS");
+
+      request.response.statusCode = HttpStatus.OK;
   		
   		if (request.uri.path == '/personagens') {
   			var contents = await new File('personagens.txt').readAsString();
